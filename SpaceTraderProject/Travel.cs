a3791faeb4586672a0da1866d0974a53d4be12fa
Zachery.Altom <zack.altom@outlook.com>
2018-09-02 23:00:51 -0400

    Added a travel class that displays instances of planets

---
+++ SpaceTraderProject/Travel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTraderProject
+{
+    class Travel
+    {
+        public void DisplayPlanets()
+        {
+            Planets[] Planet = new Planets[8];
+            Planet[0] = new Planets("Earth", 0, 0);
+            Planet[1] = new Planets("Pluto", 15, 20);
+            Planet[2] = new Planets("Uranus", -10, -10);
+            Planet[3] = new Planets("Planet Hollywood", 35, 30);
+            Planet[4] = new Planets("Happy Planet", 100, -75);
+            Planet[5] = new Planets("Alpha Centauri", 5, -10);
+            Planet[6] = new Planets("M63", 16, 3);
+            Planet[7] = new Planets("P34", -9, 6);
+
+            foreach (Planets planets in Planet)
+            {
+                Console.WriteLine(Planet);
+                Console.ReadLine();
+            }
+        } 
+        
+    }
+}
